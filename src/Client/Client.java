3c4413f1b88dfb8d62f527384c6298f01e9b8445
patricwestman <patrik.west@gmail.com>
2025-11-25 15:12:25 +0100

    Client (#8)
    * Initial commit Client
    
    * Changes to initial commit
    
    * Initial commit Client
    
    * Changes to initial commit. Replaced if statement with while loop.
    
    * Removed variable cl.
    
    * Removed variable cl added scanner to try with resources.

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -5,23 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
     public Client() {
-        try (Socket socket = new Socket("localhost", 8888)) {
+        try (Socket socket = new Socket("localhost", 8888);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner sc = new Scanner(System.in);
 
-            String question = in.readLine();
-            System.out.println("Question: " + question);
+        ){
 
-            for (int i = 0; i < 4; i++) {
-                String s = in.readLine();
-                System.out.println(s);
+            String question;
+            String answer;
+
+            question = in.readLine();
+            System.out.println(question);
+            while ((answer = sc.nextLine()) != null) {
+                out.println(answer);
+                System.out.println("Sent to Server: " + answer);
+
+                String result = in.readLine();
+                System.out.println("Rätt svar : " + result);
             }
-
-
 
         } catch(
                 IOException e) {
@@ -30,9 +37,6 @@ public class Client {
     }
 
     void main() {
-
     }
-
-
 
 }
