18dd28d383f50b4b3de28a10368aa2eb9db24ff5
patricwestman <patrik.west@gmail.com>
2025-11-25 11:04:11 +0100

    Initial commit Client

---
+++ src/Client/Client.java
@@ -0,0 +1,38 @@
+package Client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public Client() throws IOException {
+        try (Socket socket = new Socket("localhost", 8888);) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String question = in.readLine();
+            System.out.println("Question: " + question);
+
+            for (int i = 0; i < 4; i++) {
+                String s = in.readLine();
+                System.out.println(s);
+            }
+
+
+
+    } catch(
+    IOException e) {
+        throw new RuntimeException(e);
+    }
+}
+
+    void main() {
+
+    }
+
+
+
+}
