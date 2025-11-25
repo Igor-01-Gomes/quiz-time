9a5d5fdbc3680d608df1e2941f48c08f888517f3
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2025-11-25 11:10:48 +0100

    Added String to outputs.

--- src/Server/Protocol.java
+++ src/Server/Protocol.java
@@ -3,13 +3,13 @@ package Server;
 public class Protocol {
     private final int CONNECTED = 0;
     private final int INTHELOOP = 1;
-
+    Database db = new Database();
     protected int state = CONNECTED;
 
     public String getOutput (String fromCLient) {
         if (state == CONNECTED) {
             state = INTHELOOP;
-            return "Fråga: ";
+            return "Fråga :" + db.getQuestion() +"1." + db.getOptionOne() + "2." + db.getOptionTwo() +"\n"+ "3." + db.getOptionThree() + "4." + db.getOptionFour();
         } else if (state == INTHELOOP) {
             return db.getIfCorrect (fromClient.trim());
 
