8feb37f1194d35668045064275f01608a9df1de4
Mathilde-akerlind <mathilde.akerlind@gmail.com>
2025-12-01 20:39:16 +0100

    check correct answers and move to round frame after questions are answered

--- src/gui/QuestionPanel.java
+++ src/gui/QuestionPanel.java
@@ -9,13 +9,23 @@ import java.util.List;
 
 public class QuestionPanel extends JPanel {
 
+    private MainFrame frame;
+
     private List<Questions> questions = QuestionRepository.getQuestions();
     private int currentIndex = 0;
+
+    private int correctAnswers = 0;
+    private int totalAnswered = 0;
+    private int questionInRound = 0;
+    private int questionsPerRound = 2;
+    private int currentRound = 1;
 
     private JLabel questionLabel = new JLabel("", SwingConstants.CENTER);
     private JButton[] answerButtons = new JButton[4];
 
     public QuestionPanel(MainFrame frame) {
+        this.frame = frame;
+
         setLayout(new BorderLayout());
 
         questionLabel.setFont(new Font("Arial", Font.BOLD, 20));
@@ -28,15 +38,25 @@ public class QuestionPanel extends JPanel {
             answersPanel.add(answerButtons[i]);
 
             int finalI = i;
-            answerButtons[i].addActionListener(e -> {
-                // För nu bara gå vidare till nästa fråga
-                nextQuestion();
-            });
+            answerButtons[i].addActionListener(e -> handleAnswer(finalI));
         }
 
         add(answersPanel, BorderLayout.CENTER);
 
         loadQuestion();
+    }
+
+    private void handleAnswer(int selectedIndex) {
+        Questions q = questions.get(currentIndex);
+        int correct = q.getCorrectIndex();
+
+        totalAnswered++;
+
+        if (selectedIndex == correct) {
+            correctAnswers++;
+        }
+
+        nextQuestion();
     }
 
     private void loadQuestion() {
@@ -50,10 +70,33 @@ public class QuestionPanel extends JPanel {
     }
 
     private void nextQuestion() {
+        questionInRound++;
+
+        if (questionInRound >= questionsPerRound) {
+
+            int total = totalAnswered;
+            int correct = correctAnswers;
+
+            String text = "<html><center>Rond " + currentRound + " klar!<br>" +
+                    "Du fick " + correct + " av " + total + " rätt.</center></html>";
+
+            RoundPanel round = frame.getRoundPanel();
+            round.setSummaryText(text);
+
+            currentRound++;
+            questionInRound = 0;
+            correctAnswers = 0;
+            totalAnswered = 0;
+
+            frame.showPanel("round");
+            return;
+        }
+
         currentIndex++;
         if (currentIndex >= questions.size()) {
             currentIndex = 0;
         }
+
         loadQuestion();
     }
 }
