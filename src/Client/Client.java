a508716c2948740fdd151a3337416817169f64e8
patricwestman <patrik.west@gmail.com>
2025-12-02 17:50:50 +0100

    Replaced try with resources with try-catch, changed name on String (#24)

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -10,29 +10,32 @@ import java.util.Scanner;
 public class Client {
 
     public Client() {
-        try (Socket socket = new Socket("localhost", 8888);
+        try {
+            Socket socket = new Socket("localhost", 8888);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Scanner sc = new Scanner(System.in);
-
-        ){
 
             String categoryQuestion;
             String answer;
 
             categoryQuestion = in.readLine();
             System.out.println(categoryQuestion);
+
             while ((answer = sc.nextLine()) != null) {
                 out.println(answer);
                 System.out.println("Sent to Server: " + answer);
 
                 String result = in.readLine();
+                if (result == null) {
+                    System.out.println("An error occurred.");
+                    break;
+                }
                 System.out.println(result);
             }
 
-        } catch(
-                IOException e) {
-            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Unexpected error " + e.getMessage());
         }
     }
 
