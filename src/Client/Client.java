9c615c671e776041d068aa769f2e0e9a2356af16
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2025-12-03 11:31:03 +0100

    removed out commented things in client (#29)

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -21,27 +21,7 @@ public class Client extends JFrame {
             socket = new Socket("localhost", 8888);
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-
-//            Scanner sc = new Scanner(System.in);
-
-//            String categoryQuestion;
-//            String answer;
-//
-//            categoryQuestion = in.readLine();
-//            System.out.println(categoryQuestion);
-//
-//            while ((answer = sc.nextLine()) != null) {
-//                out.println(answer);
-//                System.out.println("Sent to Server: " + answer);
-//
-//                String result = in.readLine();
-//                if (result == null) {
-//                    System.out.println("An error occurred.");
-//                    break;
-//                }
-//                System.out.println(result);
-//            }
+            
 
         } catch (IOException e) {
             System.out.println("Unexpected error " + e.getMessage());
